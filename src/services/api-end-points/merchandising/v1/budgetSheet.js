2d12bd812f27bcafe1c4d7e4740710e45d177568
iqbal <1257148iqbal@gmail.com>
2022-12-07 15:10:46 +0600

    budget

--- src/services/api-end-points/merchandising/v1/budgetSheet.js
+++ src/services/api-end-points/merchandising/v1/budgetSheet.js
@@ -11,5 +11,5 @@ export const BUDGET_SHEET_API = {
   fetch_budget_sheet_by_Id: budgetId => `/api/reports/BudgetSheets/Budgets/${budgetId}/BudgetSheet`,
 
   //For RDLC Report
-  fetch_budget_sheet_by_Id_rdlc: budgetId => `Reports/Merchandising/${budgetId}/BudgetSheet`
+  fetch_budget_sheet_by_Id_rdlc: ( budgetId ) => `Reports/Merchandising/${budgetId}/BudgetSheet`
 };
